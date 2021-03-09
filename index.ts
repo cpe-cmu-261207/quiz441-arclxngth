9b0746b606b36febd5d4847c916c7a3fd904a03e
arclxngth <59277471+arclxngth@users.noreply.github.com>
2021-03-09 14:22:38 +0700

    Implemented register

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import fs from 'fs'
 import { body, query, validationResult } from 'express-validator'
 
 const app = express()
@@ -32,7 +33,26 @@ app.post('/register',
   (req, res) => {
 
     const { username, password, firstname, lastname, balance } = req.body
-  })
+    const newUser = { username, password, firstname, lastname, balance };
+
+    const buffer = fs.readFileSync("./db.json", { encoding: "utf-8" });
+    const data = JSON.parse(buffer);
+
+    const isExistUser = data.users.find((value: { username: any }) => value.username === username);
+
+    if(isExistUser){
+      return res.status(400).json({
+        message: "Username is already in used"
+      })
+    }
+
+    data.users.push(newUser);
+    fs.writeFileSync("./db.json", JSON.stringify(data));
+
+    return res.status(200).json({
+      message: "Register successfully"
+    })
+  })  
 
 app.get('/balance',
   (req, res) => {
